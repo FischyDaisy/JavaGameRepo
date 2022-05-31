7a37f813c36ac581bba1a03d6778d0878b819d02
cf838 <cf838@drexel.edu>
2022-05-31 13:49:38 -0400

    5/31/2022
    Added more functions to newtonbody. Debating whether or not to get rid of the newton collision/body sub classes

--- JavaGame/src/com/newton/NewtonBody.java
+++ JavaGame/src/com/newton/NewtonBody.java
@@ -1,8 +1,7 @@
 package com.newton;
 
 
-import com.newton.generated.NewtonApplyForceAndTorque;
-import com.newton.generated.Newton_h;
+import com.newton.generated.*;
 
 import jdk.incubator.foreign.*;
 
@@ -121,6 +120,117 @@ public abstract class NewtonBody {
 	
 	public void setLinearDamping(float linearDamp) {
 		Newton_h.NewtonBodySetLinearDamping(address, linearDamp);
+	}
+	
+	public void setAngularDamping(float[] angularDamp, SegmentAllocator allocator) {
+		MemorySegment dampSeg = allocator.allocateArray(Newton_h.C_FLOAT, angularDamp);
+		Newton_h.NewtonBodySetAngularDamping(address, dampSeg);
+	}
+	
+	public void setCollision(NewtonCollision collision) {
+		Newton_h.NewtonBodySetCollision(address, collision.address);
+	}
+	
+	public void setCollisionScale(float scaleX, float scaleY, float scaleZ) {
+		Newton_h.NewtonBodySetCollisionScale(address, scaleX, scaleY, scaleZ);
+	}
+	
+	public int getSleepState() {
+		return Newton_h.NewtonBodyGetSleepState(address);
+	}
+	
+	public void setSleepState(int state) {
+		Newton_h.NewtonBodySetSleepState(address, state);
+	}
+	
+	public int getAutoSleepState() {
+		return Newton_h.NewtonBodyGetAutoSleep(address);
+	}
+	
+	public void setAutoSleepState(int state) {
+		Newton_h.NewtonBodySetAutoSleep(address, state);
+	}
+	
+	public int getFreezeState() {
+		return Newton_h.NewtonBodyGetFreezeState(address);
+	}
+	
+	public void setFreezeState(int state) {
+		Newton_h.NewtonBodySetFreezeState(address, state);
+	}
+	
+	public int getGyroscopicTorque() {
+		return Newton_h.NewtonBodyGetGyroscopicTorque(address);
+	}
+	
+	public void setGyroscopicTorque(int state) {
+		Newton_h.NewtonBodySetGyroscopicTorque(address, state);
+	}
+	
+	public void setDestructorCallback(NewtonBodyDestructor callback, ResourceScope scope) {
+		NativeSymbol callbackFunc = NewtonBodyDestructor.allocate(callback, scope);
+		Newton_h.NewtonBodySetDestructorCallback(address, callbackFunc);
+	}
+	
+	public NewtonBodyDestructor getDestructorCallback(ResourceScope scope) {
+		MemoryAddress funcAddress = Newton_h.NewtonBodyGetDestructorCallback(address);
+		return NewtonBodyDestructor.ofAddress(funcAddress, scope);
+	}
+	
+	public void setTransformCallback(NewtonSetTransform callback, ResourceScope scope) {
+		NativeSymbol callbackFunc = NewtonSetTransform.allocate(callback, scope);
+		Newton_h.NewtonBodySetTransformCallback(address, callbackFunc);
+	}
+	
+	public NewtonSetTransform getTransformCallback(ResourceScope scope) {
+		MemoryAddress funcAddress = Newton_h.NewtonBodyGetTransformCallback(address);
+		return NewtonSetTransform.ofAddress(funcAddress, scope);
+	}
+	
+	public void setForceAndTorqueCallback(NewtonApplyForceAndTorque callback, ResourceScope scope) {
+		NativeSymbol callbackFunc = NewtonApplyForceAndTorque.allocate(callback, scope);
+		Newton_h.NewtonBodySetForceAndTorqueCallback(address, callbackFunc);
+	}
+	
+	public NewtonApplyForceAndTorque getForceAndTorqueCallback(ResourceScope scope) {
+		MemoryAddress funcAddress = Newton_h.NewtonBodyGetForceAndTorqueCallback(address);
+		return NewtonApplyForceAndTorque.ofAddress(funcAddress, scope);
+	}
+	
+	public int getID() {
+		return Newton_h.NewtonBodyGetID(address);
+	}
+	
+	public void setUserData(Addressable data) {
+		Newton_h.NewtonBodySetUserData(address, data);
+	}
+	
+	public MemoryAddress getUserData() {
+		return Newton_h.NewtonBodyGetUserData(address);
+	}
+	
+	public NewtonWorld getWorld() {
+		return NewtonWorld.wrap(Newton_h.NewtonBodyGetWorld(address));
+	}
+	
+	public NewtonCollision getCollision() {
+		return NewtonCollision.wrap(Newton_h.NewtonBodyGetCollision(address));
+	}
+	
+	public int getMaterialGroupID() {
+		return Newton_h.NewtonBodyGetMaterialGroupID(address);
+	}
+	
+	public int getSerializedID() {
+		return Newton_h.NewtonBodyGetSerializedID(address);
+	}
+	
+	public int getContinuousCollisionMode() {
+		return Newton_h.NewtonBodyGetContinuousCollisionMode(address);
+	}
+	
+	public int getJointRecursiveCollision() {
+		return Newton_h.NewtonBodyGetJointRecursiveCollision(address);
 	}
 	
 	public void destroy() {
